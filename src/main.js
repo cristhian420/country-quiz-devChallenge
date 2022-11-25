c3f7f825ae0ab3c5525defca34e51b94626bf067
Cristhian Salas <cristhian.sm91@gmail.com>
2022-11-25 01:03:33 -0600

    :art: [ADD] logic for know correct answer

--- src/main.js
+++ src/main.js
@@ -6,7 +6,11 @@ const answer1 = document.getElementById('answer-1');
 const answer2 = document.getElementById('answer-2');
 const answer3 = document.getElementById('answer-3');
 const answer4 = document.getElementById('answer-4');
-console.log(answer1);
+const answer1Contaniner = document.getElementById('a1');
+const questionsContainer = document.querySelector('.questions-container');
+
+
+
 const fetchData = async (urlApi) => {
     const response = await fetch(urlApi);
     const data = await response.json();
@@ -23,7 +27,7 @@ const fetchData = async (urlApi) => {
         .flat();
         const correctAnswer = questions.map(question => question.correctAnswer);
         let answerShuffled = answers.sort(() => Math.random()-0.5)
-        console.log(answerShuffled)
+
         questionTitle.innerText = questions[0].question;
         answer1.innerText = answerShuffled[0];
         answer2.innerText = answerShuffled[1];
@@ -31,9 +35,34 @@ const fetchData = async (urlApi) => {
         answer4.innerText = answerShuffled[3];
 
         console.log(correctAnswer);
-        console.log(answerShuffled[3]);
-        console.log(correctAnswer == answerShuffled[3])
-        console.log(answer1);
+
+        answer1Contaniner.addEventListener('mousedown', function isCorrect(){
+            questionsContainer.style.height = '559px';
+            const btn = document.createElement('button');
+            questionsContainer.insertAdjacentElement('beforeend', btn);
+            btn.classList.add('nextButton');
+            const icon = document.createElement('span');
+            answer1Contaniner.insertAdjacentElement('beforeend', icon);
+
+
+            if(correctAnswer == answerShuffled[0]) {
+                answer1Contaniner.classList.add('correct');
+                icon.classList.add('correctLogo');
+            } else {
+                answer1Contaniner.classList.add('incorrect');
+                icon.classList.add('incorrectLogo');
+                answerShuffled.forEach(element => {
+                    if(correctAnswer == element){
+                        element.classList.add('correct');
+                    }
+                });
+                console.log(indx);
+            }
+
+        })
+
+      
+
 
     } catch (error) {
         throw new Error(error)
@@ -43,8 +72,3 @@ const fetchData = async (urlApi) => {
 
 )();
 
-
-
-const isCorrect = (answerChoseen) => {
-    correctAnswer === [answerChoseen]
-}
